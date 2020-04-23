dd67131b6c8c719c28bfea0caa931b04b665a9d3
Justin Crawford <bikerhiker10@gmail.com>
2020-04-23 18:31:28 -0400

    scatter plot

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 
 def generate_table(dataframe, max_rows=10):
     return html.Table([
@@ -26,7 +26,7 @@ colors = {
     'text': '#7FDBFF'
 }
 
-app.layout = html.Div(children=[
+app.layout = html.Div(style={'margin': '5em'}, children=[
     html.H1(children='Hello Dash', style={
         'textAlign': 'center',
     }),
@@ -35,12 +35,29 @@ app.layout = html.Div(children=[
         '''),
 
     dcc.Graph(
-        id='example-graph',
+        id='life-exp-vs-gdp',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                dict(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
             ],
+            'layout': dict(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
         }
     ),
     html.H4(children='US Agriculture Exports (2011)'),
