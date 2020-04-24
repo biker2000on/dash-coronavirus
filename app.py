20507d01ff79b7b8ac8e5238084d9eb5c688bf01
Justin Crawford <bikerhiker10@gmail.com>
2020-04-23 21:08:14 -0400

    graph callback

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -26,14 +28,59 @@ app.layout = html.Div(style={'margin': '5em'}, children=[
         Dash: A web application framework for Python
         '''),
     dcc.Markdown(markdown_text),
-    dcc.Input(id='my-id', value='initial value', type='text'),
-    html.Div(id='my-div')
+    # dcc.Input(id='my-id', value='initial value', type='text'),
+    # html.Div(id='my-div'),
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        id='year-slider',
+        min=df['year'].min(),
+        max=df['year'].max(),
+        value=df['year'].min(),
+        marks={str(year): str(year) for year in df['year'].unique()},
+        step=None
+    )
 ])
 
+# @app.callback(
+#     Output(component_id='my-div', component_property='children'),
+#     [Input(component_id='my-id', component_property='value')]
+# )
+
 @app.callback(
-    Output(component_id='my-div', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
+    Output('graph-with-slider', 'figure'),
+    [Input('year-slider', 'value')]
 )
+
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+    traces = []
+    for i in filtered_df.continent.unique():
+        df_by_continent = filtered_df[filtered_df['continent'] == i]
+        traces.append(dict(
+            x=df_by_continent['gdpPercap'],
+            y=df_by_continent['lifeExp'],
+            text=df_by_continent['country'],
+            mode='markers',
+            opacity=0.7,
+            marker={
+                'size': 15,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name=i
+        ))
+
+    return {
+        'data': traces,
+        'layout': dict(
+            xaxis={'type': 'log', 'title': 'GDP Per Capita',
+                   'range':[2.3, 4.8]},
+            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 1},
+            hovermode='closest',
+            transition = {'duration': 500},
+        )
+    }
 
 def update_output_div(input_value):
     return 'You\'ve entered "{}"'.format(input_value)
