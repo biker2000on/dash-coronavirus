15d7de27e6ab6c579a3e6f9b0cfa179d0ccacdd0
Justin Crawford <bikerhiker10@gmail.com>
2020-04-24 10:46:38 -0400

    multiple outputs, single input

--- app.py
+++ app.py
@@ -10,110 +10,30 @@ available_indicators = df['Indicator Name'].unique()
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-markdown_text = '''
-### Dash and Markdown
-
-Dash apps can be written in Markdown.
-Dash uses the [CommonMark](http://commonmark.org/)
-specification of Markdown.
-
-Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
-if this is your first introduction to Markdown!
-'''
-
-app.layout = html.Div(style={'margin': '5em'}, children=[
-    html.H1(children='Hello Dash', style={
-        'textAlign': 'center',
-    }),
-    html.Div(children='''
-        Dash: A web application framework for Python
-        '''),
-    dcc.Markdown(markdown_text),
-    html.Div([
-
-        html.Div([
-            dcc.Dropdown(
-                id='xaxis-column',
-                options=[{'label': i, 'value': i}
-                         for i in available_indicators],
-                value='Fertility rate, total (births per woman)'
-            ),
-            dcc.RadioItems(
-                id='xaxis-type',
-                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                value='Linear',
-                labelStyle={'display': 'inline-block'}
-            )
-        ],
-            style={'width': '48%', 'display': 'inline-block'}),
-
-        html.Div([
-            dcc.Dropdown(
-                id='yaxis-column',
-                options=[{'label': i, 'value': i}
-                         for i in available_indicators],
-                value='Life expectancy at birth, total (years)'
-            ),
-            dcc.RadioItems(
-                id='yaxis-type',
-                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                value='Linear',
-                labelStyle={'display': 'inline-block'}
-            )
-        ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+app.layout = html.Div([
+    dcc.Input(
+        id='num-multi',
+        type='number',
+        value=5
+    ),
+    html.Table([
+        html.Tr([html.Td(['x', html.Sup(2)]), html.Td(id='square')]),
+        html.Tr([html.Td(['x', html.Sup(3)]), html.Td(id='cube')]),
+        html.Tr([html.Td([2, html.Sup('x')]), html.Td(id='twos')]),
+        html.Tr([html.Td([3, html.Sup('x')]), html.Td(id='threes')]),
+        html.Tr([html.Td(['x', html.Sup('x')]), html.Td(id='x^x')]),
     ]),
-
-    dcc.Graph(id='indicator-graphic'),
-
-    dcc.Slider(
-        id='year--slider',
-        min=df['Year'].min(),
-        max=df['Year'].max(),
-        value=df['Year'].max(),
-        marks={str(year): str(year) for year in df['Year'].unique()},
-        step=None
-    )
 ])
 
 @app.callback(
-    Output('indicator-graphic', 'figure'),
-    [Input('xaxis-column', 'value'),
-     Input('yaxis-column', 'value'),
-     Input('xaxis-type', 'value'),
-     Input('yaxis-type', 'value'),
-     Input('year--slider', 'value')])
-def update_graph(xaxis_column_name, yaxis_column_name,
-                 xaxis_type, yaxis_type,
-                 year_value):
-    dff = df[df['Year'] == year_value]
-
-    return {
-        'data': [dict(
-            x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
-            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
-            text=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'],
-            mode='markers',
-            marker={
-                'size': 15,
-                'opacity': 0.5,
-                'line': {'width': 0.5, 'color': 'white'}
-            }
-        )],
-        'layout': dict(
-            xaxis={
-                'title': xaxis_column_name,
-                'type': 'linear' if xaxis_type == 'Linear' else 'log'
-            },
-            yaxis={
-                'title': yaxis_column_name,
-                'type': 'linear' if yaxis_type == 'Linear' else 'log'
-            },
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
-            hovermode='closest',
-            transition = {'duration': 500}
-        )
-    }
-
+    [Output('square', 'children'),
+     Output('cube', 'children'),
+     Output('twos', 'children'),
+     Output('threes', 'children'),
+     Output('x^x', 'children')],
+    [Input('num-multi', 'value')])
+def callback_a(x):
+    return x**2, x**3, 2**x, 3**x, x**x
 
 if __name__ == '__main__':
     app.run_server(debug=True)
